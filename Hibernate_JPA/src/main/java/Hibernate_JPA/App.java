9240bb5d8ed4f8fe02aa211ea27dedd996af598e
Mohamed Aziz Bibi <Medazizbibi@outlook.fr>
2018-11-08 13:24:23 +0100

    Third commit

---
+++ Hibernate_JPA/src/main/java/Hibernate_JPA/App.java
@@ -0,0 +1,13 @@
+package Hibernate_JPA;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ Hibernate_JPA/src/test/java/Hibernate_JPA/AppTest.java
@@ -0,0 +1,20 @@
+package Hibernate_JPA;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
