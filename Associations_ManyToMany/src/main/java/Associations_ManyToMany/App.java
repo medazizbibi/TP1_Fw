f3076513479d2ef6a3a9c9565790c884649a2a43
Mohamed Aziz Bibi <Medazizbibi@outlook.fr>
2018-11-08 13:21:01 +0100

    Second commit

---
+++ Associations_ManyToMany/src/main/java/Associations_ManyToMany/App.java
@@ -0,0 +1,13 @@
+package Associations_ManyToMany;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
